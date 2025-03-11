15a297da78c79f5f2768292f77b73578683a8f26
Aira Mercado <airamercado@outlook.ph>
2025-03-12 01:26:45 +0800

    updates

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const menu = document.querySelector("#menu");
 const nav = document.querySelector(".links");
 
 menu.onClick = () => {
-    menu.classList.toggle('bx-x');
-    nav.classList.toggle('active');
-}
+  menu.classList.toggle("bx-x");
+  nav.classList.toggle("active");
+};
